c36fef834154801ade13c78e60e032238cdaf6e8
David Murray <54763683+thedavidmurray@users.noreply.github.com>
2024-01-30 23:36:45 -0500

    Update check_somethingNFT.sol

--- contracts/src/check_somethingNFT.sol
+++ contracts/src/check_somethingNFT.sol
@@ -4,10 +4,15 @@
 
 pragma solidity ^0.8.20;
 
-import {ERC721} from "../lib/openzeppelin-contracts/contracts/token/ERC721//ERC721.sol";
-import {Ownable} from "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
-import {Strings} from "../lib/openzeppelin-contracts/contracts/utils/Strings.sol";
-import {Base64} from "./Base64.sol";
+//import {ERC721} from "../lib/openzeppelin-contracts/contracts/token/ERC721//ERC721.sol";
+//import {Ownable} from "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+//import {Strings} from "../lib/openzeppelin-contracts/contracts/utils/Strings.sol";
+//import {Base64} from "./Base64.sol";
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
 
 
 contract check_somethingNFT is ERC721, Ownable {
